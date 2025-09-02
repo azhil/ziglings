affdf97dd07d14ac73c0e1eeee2be76ab9ff7001
Alexey Zhiltsov <azhil89@gmail.com>
2025-09-02 16:47:15 +0200

    fix: 059-63

--- exercises/059_integers.zig
+++ exercises/059_integers.zig
@@ -3,11 +3,16 @@
 // formats. These are all the same value:
 //
 //     const a1: u8 = 65;          // decimal
+//     65 / 16 = 4 (1)
 //     const a2: u8 = 0x41;        // hexadecimal
+//     65 / 8 = 8 (1) / 8 = 1 (0)
 //     const a3: u8 = 0o101;       // octal
+//     65 / 2 = 32 (1) / 2 = 16 (0) / 2 = 8 (0) / 2 = 4 (0) / 2 = 2 (0) / 2 = 1 (0)
 //     const a4: u8 = 0b1000001;   // binary
 //     const a5: u8 = 'A';         // ASCII code point literal
+//     ???
 //     const a6: u16 = '\u{0041}'; // Unicode code points can take up to 21 bits
+//     ???
 //
 // You can also place underscores in numbers to aid readability:
 //
@@ -20,9 +25,9 @@ const print = @import("std").debug.print;
 
 pub fn main() void {
     const zig = [_]u8{
-        0o131, // octal
-        0b1101000, // binary
-        0x66, // hex
+        0o131 + 1, // octal
+        0b1101000 + 1, // binary
+        0x66 + 1, // hex
     };
 
     print("{s} is cool.\n", .{zig});
